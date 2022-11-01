1c42993deec17a78ff65f8cc6ca95bbe83e85ff0
AnitaTT <anita.tsai@fazzfinancial.com>
2022-11-01 22:04:20 +0800

    revised the create api not to send the message

--- slack-announcement-scheduler/app/controllers/api/v1/messages_controller.rb
+++ slack-announcement-scheduler/app/controllers/api/v1/messages_controller.rb
@@ -4,13 +4,14 @@ class Api::V1::MessagesController < ApplicationController
       @messages = Message.all
       render json: {
         data: @messages.map{ |message| 
-        {name: message.name, department: message.department, target_announce_date: message.target_announce_date, id: message.id }
+        {name: message.name, department: message.department, target_announce_date: message.target_announce_date, id: message.id, approval: message.approval }
       }}
     end 
 
     def create 
       @message = Message.new(message_params)
       @message.save
+      render json:{ message: 'Your announcement request is submitted pending approval'}, status: :ok
     end
 
     def show
@@ -22,19 +23,35 @@ class Api::V1::MessagesController < ApplicationController
 
     def update 
       @message = Message.find(params[:id])
-      @message.update_attributes( approval: "Approved" )
+      @message.update(message_params)
       render json: {
         data: @message
       }
+
+    end
+
+    def accept
+      @message = Message.find(params[:id])
+      @message.status = :accepted
+      @message.save
+
       SendMessageToSlackJob.set(wait_until: Time.parse(@message.target_announce_date.to_s)).perform_later(@message)
-      render json: { message: "OK"}, status: :ok
+      render json: { message: 'OK' }, status: :ok
+
+    end
+
+    def reject
+      @message = Message.find(params[:id])
+      @message.status = :rejected
+      @message.save
+      reder json: { message: 'The announcement request has been rejected, please contact People Team' }, status: :ok
 
     end
 
     def destroy
       @message = Message.find(params[:id])
       @message.destroy
-      render json: { message: "The announcement has been deleted"}, status: :ok
+      render json: { message: 'The announcement has been deleted'}, status: :ok
 
     end
 
