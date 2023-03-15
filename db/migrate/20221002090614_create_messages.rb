3f34d2658b726098b1dc585c18edb31992e327d6
AnitaTT <anita.tsai@fazzfinancial.com>
2023-03-15 20:36:12 +0800

    updated migration file

--- db/migrate/20221002090614_create_messages.rb
+++ db/migrate/20221002090614_create_messages.rb
@@ -4,7 +4,7 @@ class CreateMessages < ActiveRecord::Migration[7.0]
       t.text :name
       t.text :description
       t.text :department
-      t.text :when_to_announce
+      t.date :when_to_announce
 
       t.timestamps
     end
