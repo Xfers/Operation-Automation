4583448300881f34bdda647827a3750e66990372
Wei-Yi Chiu <weiyi@xfers.com>
2023-05-17 00:31:21 +0800

    remove tokens

--- lib/weiyiai.rb
+++ lib/weiyiai.rb
@@ -8,12 +8,12 @@ require_relative 'weiyiai/client'
 # require "weiyiai"
 #
 # Slack.configure do |config|
-#   config.token = 'xapp-1-A0577R9J0HM-5257456449125-4c40d943af6192be723058c0baeab2c4e143aa065f0231557ca4b53ee4ebf13b'
+#   config.token = 'TOKEN'
 # end
 # @weiyiai = WeiyiAI::Client.new(
 #   openai_configs: {
-#     access_token: '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',
-#     organization_id: 'org-Zrqf615iiugEtT3GNWBSkD3E'
+#     access_token: 'TOKEN',
+#     organization_id: 'ORG_ID'
 #   },
 #   slack_configs: {
 #     connection: Slack::Web::Client.new,
